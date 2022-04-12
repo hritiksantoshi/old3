8dee37a08159406a1a158cb8d6717e2654b447e0
hritiksantoshi <hritiksantoshi99@gmail.com>
2022-04-12 10:35:05 +0530

    second commit

--- server/v1/controllers/UserController.js
+++ server/v1/controllers/UserController.js
@@ -14,7 +14,7 @@ async function register(req, res){
             isDeleted: false
         });
         if(emailUser){
-            res.send("EMAIL_ALREADY_EXIST");
+            res.send("EMAIL_ALREADY_EXISTS");
         }
 
         if(req.body.phoneNo){
